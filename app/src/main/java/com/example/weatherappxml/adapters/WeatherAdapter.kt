25fe66c7fb7fdb10a31e2fabb6a4e9f903d1a4fd
Stepan Kirsanov <oxidantered@gmail.com>
2023-05-17 22:44:10 +0300

    работа с RecyclerView

---
+++ app/src/main/java/com/example/weatherappxml/adapters/WeatherAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.weatherappxml.adapters
+
+import android.view.MenuItem
+import android.view.View
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weatherappxml.databinding.ListItemBinding
+
+// ...
+class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
+    class  Holder(view: View) : RecyclerView.ViewHolder(view){
+        val binding = ListItemBinding.bind(view)
+
+        fun bind(item: WeatherModel) = with(binding){
+            tvDate.text = item.time
+            tvCondition.text = item.condition
+            tvTemp.text = item.currentTemp
+        }
+    }
+}
