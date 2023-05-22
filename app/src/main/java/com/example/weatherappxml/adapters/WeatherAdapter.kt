663b60380ab18e5c465403f0e28ec683cfe033be
Stepan Kirsanov <oxidantered@gmail.com>
2023-05-22 23:57:50 +0300

    работа с Adapter

--- app/src/main/java/com/example/weatherappxml/adapters/WeatherAdapter.kt
+++ app/src/main/java/com/example/weatherappxml/adapters/WeatherAdapter.kt
@@ -1,20 +1,46 @@
 package com.example.weatherappxml.adapters
 
+import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.weatherappxml.R
 import com.example.weatherappxml.databinding.ListItemBinding
 
 // ...
-class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
-    class  Holder(view: View) : RecyclerView.ViewHolder(view){
+class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>(Comparator()) {
+    // Заполнение и сохранение раметки
+    class Holder(view: View) : RecyclerView.ViewHolder(view) {
         val binding = ListItemBinding.bind(view)
 
-        fun bind(item: WeatherModel) = with(binding){
+        fun bind(item: WeatherModel) = with(binding) {
             tvDate.text = item.time
             tvCondition.text = item.condition
             tvTemp.text = item.currentTemp
         }
+    }
+
+    class Comparator : DiffUtil.ItemCallback<WeatherModel>() {
+        override fun areItemsTheSame(oldItem: WeatherModel, newItem: WeatherModel): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: WeatherModel, newItem: WeatherModel): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.list_item, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        TODO("Not yet implemented")
     }
 }
